2ea28b02a18570b2107e6724df9ac106a7924083
Cassie Jones <cassie@Cassies-MacBook-Pro.local>
2021-01-09 14:05:33 -0600

    dialing in css/html added cards on home page

--- src/app/home/skills/skills.component.ts
+++ src/app/home/skills/skills.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
+  illustration = 'I like drawing my dog. I also love drawing test test test test test';
+  graphicDesign = 'Whatever you need, I can do, test test test test test';
+  webDev = 'Brand new to this, but having a blast learning! test test test test test'
+
   constructor() { }
 
   ngOnInit(): void {
